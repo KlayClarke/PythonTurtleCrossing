4696016c0ab90b3a8db4cea807df14a458345b91
Klay Clarke <klayaclarke@gmail.com>
2021-11-18 12:37:42 -0500

    change size of cars

--- cars.py
+++ cars.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import random
 
 COLORS = ['red', 'green', 'orange', 'pink', 'blue', 'purple', 'brown']
 
@@ -6,4 +7,11 @@ COLORS = ['red', 'green', 'orange', 'pink', 'blue', 'purple', 'brown']
 class Cars(Turtle):
     def __init__(self):
         super().__init__()
+        self.penup()
+        self.shape('square')
+        self.shapesize(stretch_wid=2, stretch_len=3, outline=None)
+
+    def generate_car(self):
+        self.color(random.choice(COLORS))
+
 
