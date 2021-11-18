f1cf516556f4124efa912e267d6c5be65692c8eb
Klay Clarke <klayaclarke@gmail.com>
2021-11-18 11:54:17 -0500

    display level / score on screen

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import time
 from turtle import Screen
+from player import Player
+from scoreboard import Scoreboard
+
+player = Player()
+scoreboard = Scoreboard()
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+
+screen.listen()
+screen.onkey(player.player_move_up(), key='Up')
+
 
 game_is_on = True
 
