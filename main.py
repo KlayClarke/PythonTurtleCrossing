f5e983c36a5550bccfefc4c2fc608e4c301dfae8
Klay Clarke <klayaclarke@gmail.com>
2021-11-18 12:40:20 -0500

    if player makes it across the street, reset position and display next level

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import time
+from cars import Cars
 from turtle import Screen
 from player import Player
 from scoreboard import Scoreboard
 
-player = Player()
-scoreboard = Scoreboard()
 
 screen = Screen()
-screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.title('Turtle Crossing')
+screen.setup(width=600, height=600)
 
+player = Player()
+
+car = Cars()
+
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(player.player_move_up(), key='Up')
-
+screen.onkey(player.go_up, 'Up')
 
 game_is_on = True
 
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    if player.ycor() > 290:
+        player.reset_position()
+        scoreboard.add_level()
+
+screen.exitonclick()
