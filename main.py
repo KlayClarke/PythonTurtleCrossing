901d335823dce55b93ef51a0c850b993171a213c
Klay Clarke <klayaclarke@gmail.com>
2021-11-18 14:24:12 -0500

    when the game is on, create a car 20% of the time to prevent overbearing amount of cars on screen - make cars move along x axis

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import time
-from cars import Cars
+from car_manager import CarManager
+from car import Car
 from turtle import Screen
 from player import Player
 from scoreboard import Scoreboard
-
 
 screen = Screen()
 screen.tracer(0)
@@ -12,7 +12,7 @@ screen.setup(width=600, height=600)
 
 player = Player()
 
-car = Cars()
+car_manager = CarManager()
 
 scoreboard = Scoreboard()
 
@@ -24,16 +24,18 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.01)
     screen.update()
-    car.car_move()
+
+    car_manager.create_car()
+    car_manager.move_cars()
 
     if player.ycor() > 290:
         player.reset_position()
         scoreboard.add_level()
-        car.generate_car()
 
-    elif player.distance(car) < 20:
-        print('splat')
-        scoreboard.game_over()
-        game_is_on = False
+
+    # elif player.distance(car_manager) < 20:
+    #     print('splat')
+    #     scoreboard.game_over()
+    #     game_is_on = False
 
 screen.exitonclick()
