3577e62aea6148437e67b8ab73e207a60addfd3a
Klay Clarke <klayaclarke@gmail.com>
2021-11-18 12:39:20 -0500

    create methods to add_level text to current display and to update the display after each round

--- scoreboard.py
+++ scoreboard.py
@@ -11,3 +11,11 @@ class Scoreboard(Turtle):
         self.goto(x=-250, y=270)
         self.level = 0
         self.write(arg=f'Level: {self.level}', align='center', move=False, font=FONT)
+
+    def update_level(self):
+        self.clear()
+        self.write(arg=f'Level: {self.level}', align='center', move=False, font=FONT)
+
+    def add_level(self):
+        self.level += 1
+        self.update_level()
