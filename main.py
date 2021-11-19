0d6a7215aff052ba7bd1cefa367407bcbb28e069
Klay Clarke <klayaclarke@gmail.com>
2021-11-18 19:10:20 -0500

    change screen bg color to grey - better on the eyes

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.tracer(0)
 screen.title('Turtle Crossing')
+screen.bgcolor('grey')
 screen.setup(width=600, height=600)
 
 player = Player()
