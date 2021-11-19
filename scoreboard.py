c4a4f14e4f724aeb35ff281e24ddd8d579518672
Klay Clarke <klayaclarke@gmail.com>
2021-11-18 19:05:40 -0500

    if user loses, game resets - goes back to level 1 - but player is able to see (and strive for) the highest score

--- scoreboard.py
+++ scoreboard.py
@@ -8,13 +8,21 @@ class Scoreboard(Turtle):
         super().__init__()
         self.hideturtle()
         self.penup()
-        self.goto(x=-250, y=270)
+        self.goto(x=-150, y=270)
         self.level = 0
-        self.write(arg=f'Level: {self.level}', align='center', move=False, font=FONT)
+        with open('data.txt', mode='r') as data:
+            self.high_level = int(data.read())
+        self.write(arg=f'Level: {self.level} Highest Level: {self.high_level}' f'', align='center', move=False,
+                   font=FONT)
 
     def update_level(self):
         self.clear()
-        self.write(arg=f'Level: {self.level}', align='center', move=False, font=FONT)
+        if self.level > self.high_level:
+            self.high_level = self.level
+        with open('data.txt', mode='w') as data:
+            data.write(f'{self.high_level}')
+        self.write(arg=f'Level: {self.level} Highest Level: {self.high_level}' f'', align='center', move=False,
+                   font=FONT)
 
     def add_level(self):
         self.level += 1
@@ -22,5 +30,6 @@ class Scoreboard(Turtle):
 
     def game_over(self):
         self.clear()
-        self.goto(0, 0)
-        self.write(arg='GAME OVER', align='center', move=False, font=FONT)
+        self.level = 0
+        self.write(arg=f'Level: {self.level} Highest Level: {self.high_level}' f'', align='center', move=False,
+                   font=FONT)
